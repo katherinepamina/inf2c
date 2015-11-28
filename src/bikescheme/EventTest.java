b08fa866f00aaff297d490504a101540b41056f3
katherinepamina <katherinelin2016@u.northwestern.edu>
2015-11-28 19:18:58 +0000

    fixed equals tests

--- src/bikescheme/EventTest.java
+++ src/bikescheme/EventTest.java
@@ -27,69 +27,69 @@ public class EventTest {
     }
     @Test
     public void equals_testDateNotEqual() {
-    	assertNotEquals(
-    			new Event("31 22:10,C,i,m, a, b, c, d"),
-    			new Event("29 22:10,C,i,m, a, b, c, d") );
+    	Event e1 = new Event("31 22:10,C,i,m, a, b, c, d");
+    	Event e2 = new Event("29 22:10,C,i,m, a, b, c, d");
+    	assertFalse(e1.equals(e2));
     }
     @Test
     public void equals_testDeviceClassNotEqual() {
-    	assertNotEquals(
-    			new Event("31 22:10,C,i,m, a, b, c, d"),
-    			new Event("31 22:10,D,k,m, a, b, c, d") );
+    	Event e1 = new Event("31 22:10,C,i,m, a, b, c, d");
+    	Event e2 = new Event("31 22:10,D,k,m, a, b, c, d");
+    	assertFalse(e1.equals(e2));
     }
     @Test
     public void equals_testMessageArgsNotEqual() {
-    	assertNotEquals(
-    			new Event("31 22:10,C,i,m, a, b, c, d, e"),
-    			new Event("31 22:10,C,i,m, a, b, c, d") );
+    	Event e1 = new Event("31 22:10,C,i,m, a, b, c, d, e");
+    	Event e2 = new Event("31 22:10,C,i,m, a, b, c, d");
+    	assertFalse(e1.equals(e2));
     }
     @Test
     public void equals_ignoreWhiteSpace() {
-        assertEquals(
-                new Event("1 00:00,C,i,m, a, b, c, d"),
-                new Event("1 00:00,C,i,m, a,b,c,d") );
+        Event e1 = new Event("1 00:00,C,i,m, a, b, c, d");
+        Event e2 = new Event("1 00:00,C,i,m, a,b,c,d");
+        assertTrue(e1.equals(e2));
     }
     @Test
     public void equals_testMessageArgs() {
-    	assertNotEquals(
-    			new Event("31 22:10,C,i,m, c, b, a, d"),
-    			new Event("31 22:10,C,i,m, a, b, c, d") );
+    	Event e1 = new Event("31 22:10,C,i,m, c, b, a, d");
+    	Event e2 = new Event("31 22:10,C,i,m, a, b, c, d");
+    	assertFalse(e1.equals(e2));
     }
     @Test
     public void equals_testMessageArgs2() {
-    	assertEquals(
-    			new Event("31 22:10,C,i,m, unordered-tuples,1,first,second,c, b, a, d"),
-    			new Event("31 22:10,C,i,m, unordered-tuples,1,first,second,a, b, c, d") );
+    	Event e1 = new Event("31 22:10,C,i,m, unordered-tuples,1,first,second,c, b, a, d");
+    	Event e2 = new Event("31 22:10,C,i,m, unordered-tuples,1,first,second,a, b, c, d");
+    	assertTrue(e1.equals(e2));
     }
     @Test
     public void equals_testMessageArgs3() {
-    	assertNotEquals(
-    			new Event("31 22:10,C,i,m, unordered-tuples,1,first,second,f, b, a, d"),
-    			new Event("31 22:10,C,i,m, unordered-tuples,1,first,second,a, b, c, d") );
+    	Event e1 = new Event("31 22:10,C,i,m, unordered-tuples,1,first,second,f, b, a, d");
+    	Event e2 = new Event("31 22:10,C,i,m, unordered-tuples,1,first,second,a, b, c, d");
+    	assertFalse(e1.equals(e2));
     }
     @Test
     public void equals_testMessageArgs4() {
-    	assertNotEquals(
-    			new Event("31 22:10,C,i,m, unordered-tuples,first,second,2,a,b,c,d,e,f"),
-    			new Event("31 22:10,C,i,m, unordered-tuples,first,second,1,a,b,c,d,e,f") );
+    	Event e1 = new Event("31 22:10,C,i,m, unordered-tuples,first,second,2,a,b,c,d,e,f");
+    	Event e2 = new Event("31 22:10,C,i,m, unordered-tuples,first,second,1,a,b,c,d,e,f");
+    	assertFalse(e1.equals(e2));
     }
     @Test
     public void equals_testMessageArgs5() {
-    	assertEquals(
-    			new Event("1 00:00,C,i,m, unordered-tuples,2,first,second,a,b,c,d,e,f"),
-    			new Event("1 00:00,C,i,m, unordered-tuples,2,first,second,a,b,e,f,c,d") );
+    	Event e1 = new Event("1 00:00,C,i,m, unordered-tuples,2,first,second,a,b,c,d,e,f");
+    	Event e2 = new Event("1 00:00,C,i,m, unordered-tuples,2,first,second,a,b,e,f,c,d");
+    	assertTrue(e1.equals(e2));
     }
     @Test
     public void equals_testMessageArgs6() {
-    	assertNotEquals(
-    			new Event("31 22:10,C,i,m, unordered-tuples,2,first,second,a,c,b,d,e,f"),
-    			new Event("31 22:10,C,i,m, unordered-tuples,2,first,second,a,b,e,f,c,d") );
+    	Event e1 = new Event("31 22:10,C,i,m, unordered-tuples,2,first,second,a,c,b,d,e,f");
+    	Event e2 = new Event("31 22:10,C,i,m, unordered-tuples,2,first,second,a,b,e,f,c,d");
+    	assertFalse(e1.equals(e2));
     }
     @Test
     public void equals_testHeaderTuples() {
-    	assertNotEquals(
-    			new Event("31 22:10,C,i,m, unordered-tuples,2,one,two,a,b,c,d"),
-    			new Event("31 22:10,C,i,m, unordered-tuples,2,first,second,a,b,c,d") );
+    	Event e1 = new Event("31 22:10,C,i,m, unordered-tuples,2,one,two,a,b,c,d");
+    	Event e2 = new Event("31 22:10,C,i,m, unordered-tuples,2,first,second,a,b,c,d");
+    	assertFalse(e1.equals(e2));
     }
     
     //tests that 2 lists of diff lengths are not equal
